32aa7029f9021386b5ecad88209be20ac46455a0
llucas <lucas.lassus@capgemini.com>
2025-03-31 18:56:12 +0200

    implemented conversation history & search results into chat incremental context

--- code/chatbot.py
+++ code/chatbot.py
@@ -1,3 +1,5 @@
+import json
+from vectordb import VectorDB
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 
@@ -5,44 +7,58 @@ from langchain_core.prompts import ChatPromptTemplate
 class Chatbot:
     def __init__(
         self,
-        question: str,
-        search: str,
         theme: str,
         template: str,
         model: str = "llama3.1",
+        rag: VectorDB = None,
     ):
-        self.question = question
-        self.search_results = search
         self.theme = theme
         self.llm = OllamaLLM(model=model)
+        self.rag = rag
+        self.template = template
+        self.search_results = None
         self.context = ""
-        self.template = (
-            template
-            + f"""
-        Here is the conversation history: {self.context}
+        self.prompt = None
+        self.chain = None
 
-        Here are the search results: {self.search_results}
-
-        Question: {self.question}
-
-        Answer:
-        """
-        )
-        self.prompt = ChatPromptTemplate.from_template(self.template)
-        self.chain = self.prompt | self.llm
-
-    def start_conversation(self):
+    def handle_conversation(self):
+        # start conversation
         print(f"Welcome to the local {self.theme} chatbot! Type 'exit' to quit.")
         while True:
+            # collect user input query
             user_input = input("You: ")
+            # exit if user types 'exit'
             if user_input.lower() == "exit":
                 break
-            result = self.chain.invoke(
+            # get search retrieval results in the vector database
+            self.search_results = self.rag.search_vector_db(
+                user_input, return_limit=3, text_output=True
+            )
+            # set user prompt with the search results
+            self.prompt = ChatPromptTemplate.from_template(
+                self.template
+                + f"""
+            Here is the conversation history: {self.context}
+
+            Here are the top search results: {self.search_results}
+
+            Question: {user_input}
+
+            Answer:
+            """
+            )
+            print(self.prompt)
+            # set the chain of response generation
+            self.chain = self.prompt | self.llm
+            # invoke the chain of response generation
+            response = self.chain.invoke(
                 {
                     "context": self.context,
                     "search_results": str(self.search_results),
                     "question": user_input,
                 }
             )
-            print("Chatbot:", result)
-            self.context += f"\nYou: {user_input}\nChatbot: {result}"
+            # print the response to user
+            print("Chatbot:", response)
+            # update the conversation history as context for next iteration
+            self.context += f"\nYou: {user_input}\nChatbot: {response}"
