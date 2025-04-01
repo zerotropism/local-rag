9b73b8da863e1335425a24bef5ad7253aadb5c5d
llucas <lucas.lassus@capgemini.com>
2025-04-01 14:30:44 +0200

    best practices: privatization of attributes, decorators & erros handling

--- code/chatbot.py
+++ code/chatbot.py
@@ -1,7 +1,7 @@
-import json
 from vectordb import VectorDB
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from decorators import handle_exception
 
 
 class Chatbot:
@@ -12,18 +12,38 @@ class Chatbot:
         model: str = "llama3.1",
         rag: VectorDB = None,
     ):
-        self.theme = theme
-        self.llm = OllamaLLM(model=model)
-        self.rag = rag
-        self.template = template
-        self.search_results = None
-        self.context = ""
-        self.prompt = None
-        self.chain = None
+        """Initialize the Chatbot class.
+        Args:
+            theme (str): Theme of the chatbot.
+            template (str): Template for the chatbot conversation.
+            model (str, optional): Model name. Defaults to "llama3.1".
+            rag (VectorDB, optional): Vector database object. Defaults to None.
+        """
+        self._theme = theme
+        self._llm = OllamaLLM(model=model)
+        self._rag = rag
+        self._template = template
+        self._search_results = None
+        self._context = ""
+        self._prompt = None
+        self._chain = None
 
+    @property
+    def theme(self, value: str):
+        """Get the theme of the chatbot
+        Args:
+            value (str): Theme of the chatbot.
+        """
+        self._theme = value
+
+    @handle_exception
     def handle_conversation(self):
+        """Handle the conversation with the chatbot.
+        This method initiates the conversation with the user and handles the interaction.
+        It uses the vector database to retrieve relevant information based on user queries.
+        """
         # start conversation
-        print(f"Welcome to the local {self.theme} chatbot! Type 'exit' to quit.")
+        print(f"Welcome to the local {self._theme} chatbot! Type 'exit' to quit.")
         while True:
             # collect user input query
             user_input = input("You: ")
@@ -31,34 +51,41 @@ class Chatbot:
             if user_input.lower() == "exit":
                 break
             # get search retrieval results in the vector database
-            self.search_results = self.rag.search_vector_db(
+            self._search_results = self._rag.search_vector_db(
                 user_input, return_limit=3, text_output=True
             )
-            # set user prompt with the search results
-            self.prompt = ChatPromptTemplate.from_template(
-                self.template
+            # we print the search results to the console for control purposes
+            print(
+                "\n\n",
+                "Vector DB search results:",
+                "\n\n",
+                self._search_results,
+                "\n\n",
+            )
+            # set user prompt as a concatenation of initial template, conversation history, the search results & user input
+            self._prompt = ChatPromptTemplate.from_template(
+                self._template
                 + f"""
-            Here is the conversation history: {self.context}
+            Here is the conversation history: {self._context}
 
-            Here are the top search results: {self.search_results}
+            Here are the top search results: {self._search_results}
 
             Question: {user_input}
 
             Answer:
             """
             )
-            print(self.prompt)
             # set the chain of response generation
-            self.chain = self.prompt | self.llm
+            self._chain = self._prompt | self._llm
             # invoke the chain of response generation
-            response = self.chain.invoke(
+            response = self._chain.invoke(
                 {
-                    "context": self.context,
-                    "search_results": str(self.search_results),
+                    "context": self._context,
+                    "search_results": str(self._search_results),
                     "question": user_input,
                 }
             )
             # print the response to user
             print("Chatbot:", response)
             # update the conversation history as context for next iteration
-            self.context += f"\nYou: {user_input}\nChatbot: {response}"
+            self._context += f"\nYou: {user_input}\nChatbot: {response}"
